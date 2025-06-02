284807cb090bfeaf56b0980b69fcc833c82cbe1b
maraad97@gmail.com <maraad97@gmail.com>
2025-06-02 20:12:46 +0200

    Create basic order confirmation page

--- src/pages/OrderComplete/OrderComplete.jsx
+++ src/pages/OrderComplete/OrderComplete.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./OrderComplete.module.css";
+import { Link } from "react-router-dom";
 
 const OrderComplete = () => {
-  return <div>OrderComplete</div>;
+  return (
+    <div className={styles.orderCompleteWrapper}>
+      <div className={styles.orderCompleteContainer}>
+        <h1 className={styles.header}>Your order has been received!</h1>
+        <p className={styles.paragraph}>
+          You will receive an email with your orders receipt.
+        </p>
+        <p className={styles.paragraph}>
+          If you encounter any issues or need help with anything, don't hesitate
+          in contacting us!
+        </p>
+
+        <Link to="/contact" className={styles.linkButton}>Contact Us!</Link>
+      </div>
+    </div>
+  );
 };
 
 export default OrderComplete;
